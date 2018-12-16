00e221b88363624071e8c40a9c34aadd0cb85d2a
Bimal Patel <bims.patel@gmail.com>
2018-12-16 14:17:26 -0500

    Made openweather api call agnostic to protocol

--- src/api/openweather.js
+++ src/api/openweather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://api.openweathermap.org/data/2.5',
+    baseURL: '//api.openweathermap.org/data/2.5',
     params: {
         appid: '386c9c97ea96d6aa43a5f204cb9ca3ca',
         units: 'imperial'
